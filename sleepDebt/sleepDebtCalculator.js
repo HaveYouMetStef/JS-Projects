eb4b27551b6fc6111389965db2e45cfe060621b6
Stef Castillo <estefan.castillo@yahoo.com>
2024-10-21 20:37:58 -0500

    Completed the last task of updated the functions to accomodate for giving the user to choice their ideal sleep number.

--- sleepDebt/sleepDebtCalculator.js
+++ sleepDebt/sleepDebtCalculator.js
@@ -26,34 +26,34 @@ function getSleepHours(day) {
     }
 }
 
-const getActualSleepHours = () => {
-    let totalHoursSlept = 0
-    totalHoursSlept += getSleepHours('monday');
-    totalHoursSlept += getSleepHours('tuesday');
-    totalHoursSlept += getSleepHours('wednesday');
-    totalHoursSlept += getSleepHours('thursday');
-    totalHoursSlept += getSleepHours('friday');
-    totalHoursSlept += getSleepHours('saturday');
-    totalHoursSlept += getSleepHours('sunday');
-    return totalHoursSlept
-}
+const getActualSleepHours = () => 8 + 7 + 6 + 7 + 7 + 10 + 10;
+    // let totalHoursSlept = 0
+    // totalHoursSlept += getSleepHours('monday');
+    // totalHoursSlept += getSleepHours('tuesday');
+    // totalHoursSlept += getSleepHours('wednesday');
+    // totalHoursSlept += getSleepHours('thursday');
+    // totalHoursSlept += getSleepHours('friday');
+    // totalHoursSlept += getSleepHours('saturday');
+    // totalHoursSlept += getSleepHours('sunday');
+    // return totalHoursSlept
 
-const idealHours = () => {
-    let idealHours = 7;
+const idealHours = (enterIdealHours) => enterIdealHours *7 ;
+// {
+//     let idealHours = 7;
 
-    return idealHours * 7;
-}
+//     return idealHours * 7;
+// }
 
 const calculateSleepDebt = () => {
-    let actualSleepHours = getActualSleepHours();
-    const idealSleepHours = idealHours();
+    const actualSleepHours = getActualSleepHours();
+    const idealSleepHours = idealHours(8);
 
     if (actualSleepHours === idealSleepHours){
         return 'You got just the right amount of sleep' + 'you slept: ' + idealHours + 'hours!';
     } else if(actualSleepHours > idealSleepHours) {
         return 'You got more than enough sleep, ' + 'you actually slept: ' + (actualSleepHours - idealSleepHours) + ' more hours than expected.';
     } else {
-        return "You should get some more sleep!" + 'you need ' + (idealSleepHours - actualSleepHours) + ' more hours of sleep.';
+        return "You should get some more sleep !" + 'you need ' + (idealSleepHours - actualSleepHours) + ' more hours of sleep.';
     }
 }
 // console.log(getSleepHours('monday'));
@@ -61,5 +61,5 @@ const calculateSleepDebt = () => {
 // console.log(getSleepHours('wednesday'));
 console.log(getActualSleepHours())
 
-console.log(idealHours())
+console.log(idealHours(8))
 console.log(calculateSleepDebt());
