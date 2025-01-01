441d019302ab1fa2fe9cc90be634e84b87bde317
Stef Castillo <estefan.castillo@yahoo.com>
2025-01-01 16:30:07 -0600

    Make the code more intuitive to ensure that random inspirational messages are presented to the user everytime

--- Message Generator/index.js
+++ Message Generator/index.js
@@ -9,11 +9,39 @@ function randomNumber(num) {
 // let messageGenerator = [];
 
 const messages = {
-    'overwhelmed': "Do not dwell on what you cannot do. Instead, focus on what you can.",
-    'defeated': `Success is not final, failure is not fatal: It is the courage to continue that counts.`,
-    'hopeful': `The future belongs to those who believe in the beauty of their dreams.`
+    'overwhelmed': [
+        "Do not dwell on what you cannot do. Instead, focus on what you can.",
+        "You don't have to see the whole staircase, just take the first step.",
+        "Start where you are. Use what you have. Do what you can."
+    ],
+    'defeated': 
+    [
+    "Success is not final, failure is not fatal: It is the courage to continue that counts.",
+    "The only limit to our realization of tomorrow will be our doubts of today.",
+    "Our greatest glory is not in never falling, but in rising every time we fall."
+    ],
+    'hopeful': 
+    [`The future belongs to those who believe in the beauty of their dreams.`,
+    "What lies behind us and what lies before us are tiny matters compared to what lies within us.",
+    "The best way to predict the future is to create it."
+    ]
 };
 
+function createRandomMessage(feeling) {
+    if(messages[feeling]) {
+        const randomIndex = randomNumber(messages[feeling].length);
+        return `You mentioned feeling ${feeling}. Here's something to inspire you: "${messages[feeling][randomIndex]}"`
+    } else {
+        return `Please let me know how you're feeling so I can give you some inspiration!`
+    }
+}
+
+console.log(createRandomMessage('overwhelmed'));
+// console.log(createRandomMessage('defeated'));
+// console.log(createRandomMessage('hopeful'));
+// console.log(createRandomMessage(''));
+
+/*
 function createRandomMessage(feeling) {
     let messageGenerator = feeling
 
@@ -28,6 +56,7 @@ function createRandomMessage(feeling) {
     }
     return messageGenerator;
 };
+/*
 
 console.log(createRandomMessage('overwhelmed'));
 console.log(createRandomMessage('defeated'));
